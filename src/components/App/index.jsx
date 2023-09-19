fef11f3d93480db95c1509d73bba5cbb6ba3ce50
nodirbek27 <nodirjon0927@gmail.com>
2023-09-19 13:50:26 +0500

    Add carousel

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '../Navbar'
+import Carousel from '../Carousel/index'
 
 function App() {
     return (
         <div>
             <Navbar />
+            <Carousel />
         </div>
     )
 }
