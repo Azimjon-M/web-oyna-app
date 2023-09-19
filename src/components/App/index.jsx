49d0d755d1cfae4030feb55b93cea4acc68ad349
Azimjon-m <azimeliboev2000@gmail.com>
2023-09-19 14:12:25 +0500

    data +

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -2,11 +2,31 @@ import React from 'react'
 import Navbar from '../Navbar'
 import Carousel from '../Carousel/index'
 
+import img1 from '../../assets/images/img-1.jpg';
+import img2 from '../../assets/images/img-2.jpg';
+import img3 from '../../assets/images/img-3.jpg';
+
 function App() {
+
+    const data = [
+        {
+            src: img1,
+            id: 0
+        },
+        {
+            src: img2,
+            id: 1,
+        },
+        {
+            src: img3,
+            id: 2,
+        },
+    ]
+
     return (
         <div>
             <Navbar />
-            <Carousel />
+            <Carousel data={data} />
         </div>
     )
 }
