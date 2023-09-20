f3771c0d73e92ed7f9a1f2cc5e341828c7dda475
Azimjon-m <azimeliboev2000@gmail.com>
2023-09-20 15:35:07 +0500

    + anyNavbar

--- src/pages/DarsJadvali/index.jsx
+++ src/pages/DarsJadvali/index.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 const DarsJadvali = () => {
- return <div>DarsJadvali</div>;
+    return (
+        <div>DarsJadvali</div>
+    )
 };
 
 export default DarsJadvali;
--- src/root/index.js
+++ src/root/index.js
@@ -1,12 +1,9 @@
 import React from 'react';
-
-import Navbar from '../components/Navbar/index';
 import { Outlet } from 'react-router-dom';
 
 const root = () => {
     return (
         <div>
-            <Navbar />
             <Outlet />
         </div>
     )
