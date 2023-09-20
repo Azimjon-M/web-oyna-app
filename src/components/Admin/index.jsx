31171c49ce27419622ccf710b37b835653aedbf9
nodirbek27 <nodirjon0927@gmail.com>
2023-09-20 14:44:32 +0500

    install axios

--- src/components/Admin/index.jsx
+++ src/components/Admin/index.jsx
@@ -1,17 +1,8 @@
-import React, {useEffect} from 'react'
+import React from 'react'
 
 const Admin = () => {
-
-    useEffect(()=>{
-      axios.get("https://newsapi.org/v2/top-headlines?country=us&apiKey=API_KEY")
-      .then((res)=> {
-        console.log(res);
-      })
-    }, [])
   return (
-    <>
-        <h1>Hello</h1>
-    </>
+    <div>Admin</div>
   )
 }
 
--- src/pages/Yangiliklar/index.jsx
+++ src/pages/Yangiliklar/index.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, {useEffect} from 'react';
+import axios from 'axios'
 
 const Yangiliklar = () => {
-    return (
-        <div>Yangiliklar</div>
-    )
+
+    useEffect(()=>{
+      axios.get("https://newsapi.org/v2/top-headlines?country=us&apiKey=API_KEY")
+      .then((res)=> {
+        console.log(res);
+      })
+    }, [])
+  return (
+    <>
+        <h1>Hello</h1>
+    </>
+  )
 }
 
 export default Yangiliklar
