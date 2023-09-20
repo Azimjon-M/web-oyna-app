46bfcfcccd0f7ae344782ce2dd57e3cca2bd69a1
nodirbek27 <nodirjon0927@gmail.com>
2023-09-20 15:35:53 +0500

    +prettier

--- src/pages/Yangiliklar/index.jsx
+++ src/pages/Yangiliklar/index.jsx
@@ -1,19 +1,19 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 import axios from 'axios'
 
 const Yangiliklar = () => {
 
-    useEffect(()=>{
-      axios.get("https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=f5d42754217c4adfb2af3219e7508901")
-      .then((res)=> {
-        console.log(res);
-      })
+    useEffect(() => {
+        axios.get("https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=f5d42754217c4adfb2af3219e7508901")
+            .then((res) => {
+                console.log(res);
+            })
     }, [])
-  return (
-    <>
-        <h1>Hello</h1>
-    </>
-  )
+    return (
+        <>
+            <h1>Hello</h1>
+        </>
+    )
 }
 
 export default Yangiliklar
