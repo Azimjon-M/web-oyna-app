11b1652b3b78ff92f79f9ba3a0239317a93285e4
Azimjon-m <azimeliboev2000@gmail.com>
2023-09-20 11:01:27 +0500

    update +

--- src/components/Carousel/index.jsx
+++ src/components/Carousel/index.jsx
@@ -27,7 +27,7 @@ const Carousel = ({ data }) => {
         className="mySwiper w-full h-[100vh]"
       >
         {data.map(item => (
-          <SwiperSlide key={item.id} className="w-full h-[100vh!important">
+          <SwiperSlide key={item.id} className="w-full h-[100vh!important]">
             <img className="w-[100%!important] h-[100%!important]" src={item.src} alt="images" />
             <div className="absolute bottom-[15%] left-[50%] translate-x-[-50%] ">
               <button className="bg-blue-500 rounded-xl px-20 py-8 text-white text-[32px] font-bold">{item.btnName}</button>
