b96b4d147ebf714e88474dda540a1acc2d1db1c7
Azimjon-m <azimeliboev2000@gmail.com>
2023-09-25 13:25:37 +0500

    upadate ++

--- src/components/Forma/index.jsx
+++ src/components/Forma/index.jsx
@@ -3,23 +3,37 @@ import { useFormik } from 'formik';
     
 const Form = () => {
 
-        const formik = useFormik({
-            initialValues: {
-            title: '',
-            body: '',
-            rasm: '',
-            },
-            onSubmit: values => {
-                fetch('http://api.kspi.uz/v1/yangilik/yangilik/', {
-                    method: "POST",
-                    headers: {'Content-Type': 'application/json'},
-                    body: JSON.stringify(values)
-                })
-                .then(result => console.log("to'g'ri:", result))
-                .catch(error => console.log('error:', error));
-            },
-            
-        });
+    const handleDownload = () => {
+        fetch('http://api.kspi.uz/v1/yangilik/yangilik/', {
+            method: "GET",
+            mode: 'no-cors',
+            headers: {'Content-Type': 'application/json', 'X-CSRFToken': 'OVGxEVVqO7R0QdPwQ7OUHBzOJeneGHhs3q8YrlswnCNarAQlcSqil8coU7gjRxMB', },
+        }).then((res) => {
+            console.log(res);
+        }).catch((err) => {
+            console.log(err);
+        })
+    }
+
+    const formik = useFormik({
+        initialValues: {
+        title: '',
+        body: '',
+        // rasm: '',
+        },
+        onSubmit: values => {
+            fetch('http://api.kspi.uz/v1/yangilik/yangilik/', {
+                method: "POST",
+                credentials: 'include',
+                mode: 'no-cors',
+                headers: {'Content-Type': 'text/plain', },
+                body: values
+            })
+            .then(result => console.log("to'g'ri:", result))
+            .catch(error => console.log('error:', error));
+        },
+        
+    });
 
         return (
             <form className='flex flex-col justify-center' onSubmit={formik.handleSubmit}>
@@ -39,16 +53,17 @@ const Form = () => {
                     onChange={formik.handleChange}
                     value={formik.values.body}
                 />
-                <label htmlFor="rasm">Rasim joylash</label>
+                {/* <label htmlFor="rasm">Rasim joylash</label>
                 <input
                     id="rasm"
                     name="rasm"
                     type="file"
                     onChange={formik.handleChange}
                     value={formik.values.rasm}
-                />
+                /> */}
             
                 <button type="submit">Submit</button>
+                <button onClick={()=> handleDownload()}>Download</button>
             </form>
         );
 }
--- src/pages/InstitutHaritasi/index.jsx
+++ src/pages/InstitutHaritasi/index.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import AnyNavbar from '../../components/anyNavbar'
-import Form from '../../components/Forma'
 
 const InstitutHaritasi = () => {
     return (
         <div>
-            {/* <AnyNavbar /> */}
+            <AnyNavbar />
             <div className='w-full flex flex-col justify-center text-center'>
                 <h1>
                     Institut haritasi
@@ -13,7 +12,9 @@ const InstitutHaritasi = () => {
                 <div className='w-full flex flex-col px-10'>
                     <div className='border border-red-600'>
                         <div className='bg-gray-300'>
-                            <Form />
+                            <h1>
+                                Yangiliklar 1
+                            </h1>
                         </div>
                     </div>
                 </div>
