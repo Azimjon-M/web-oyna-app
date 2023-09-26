5c525e9f96817683a1657141d6e7959a998cf812
nodirbek27 <nodirjon0927@gmail.com>
2023-09-26 09:10:29 +0500

    remove Anynavbar

--- src/pages/Yangiliklar/index.jsx
+++ src/pages/Yangiliklar/index.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios'
-import AnyNavbar from '../../components/anyNavbar';
 import Navbar from '../../components/Navbar';
 
 const Yangiliklar = () => {
