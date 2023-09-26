267ba8171a35360ac16da8d6659b2997b34cfd9f
nodirbek27 <nodirjon0927@gmail.com>
2023-09-26 11:13:24 +0500

    add bg-color to card

--- src/pages/Yangiliklar/index.jsx
+++ src/pages/Yangiliklar/index.jsx
@@ -33,7 +33,7 @@ const Yangiliklar = () => {
                             
                             return (
                                 <div className='my-3 shadow-lg shadow-indigo-500/50' key={idx}>
-                                    <div className="w-full flex items-center bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+                                    <div className="w-full flex items-center bg-gradient-to-r from-cyan-100 to-blue-200 border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
                                         <img src={item.urlToImage} className="m-5 object-cover w-[40%] rounded-t-lg h-60" alt="..." />
                                         <div className="flex flex-col justify-between p-4 leading-normal w-[60%]">
                                             <h5 className="mb-2 text-3xl font-bold tracking-tight text-gray-900 dark:text-white">{item.title}</h5>
