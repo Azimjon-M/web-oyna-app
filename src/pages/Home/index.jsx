aa20cc306f70fb00d8e0717591ad802b76a44f6c
Azimjon-m <azimeliboev2000@gmail.com>
2023-09-28 17:01:51 +0500

    + devises Corousel

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,31 +1,42 @@
-import React from 'react';
-import Carousel from '../../components/Carousel';
-import Navbar from '../../components/Navbar';
-import { useDispatch } from 'react-redux';
+import React from "react";
+import Carousel from "../../components/Carousel";
+import Navbar from "../../components/Navbar";
+import { useDispatch } from "react-redux";
+
+import img1 from "../../assets/images/img-1.jpg";
+import img2 from "../../assets/images/img-2.jpg";
+import img3 from "../../assets/images/img-3.jpg";
 
 const Home = () => {
+
+    const newsData = [{ src: img1 }, { src: img2 }, { src: img3 }];
 
     const dispatch = useDispatch();
     let getMonth = new Date().getMonth() + 1;
 
     if (getMonth === 1 || getMonth === 2 || getMonth === 12) {
-        dispatch({type: 'SET_DATA', payload: 'qish'})
+        dispatch({ type: "SET_DATA", payload: "qish" });
     } else if (getMonth >= 3 && getMonth <= 5) {
-        dispatch({type: 'SET_DATA', payload: 'bahor'})
+        dispatch({ type: "SET_DATA", payload: "bahor" });
     } else if (getMonth >= 6 && getMonth <= 8) {
-        dispatch({type: 'SET_DATA', payload: 'yoz'})
+        dispatch({ type: "SET_DATA", payload: "yoz" });
     } else if (getMonth >= 9 && getMonth <= 11) {
-        dispatch({type: 'SET_DATA', payload: 'kuz'})
+        dispatch({ type: "SET_DATA", payload: "kuz" });
     } else {
-        dispatch({type: 'SET_DATA', payload: 'yoz'})
-    };
+        dispatch({ type: "SET_DATA", payload: "yoz" });
+    }
 
     return (
         <div>
             <Navbar />
-            <Carousel />
+            <div className="border border-red-600 w-full h-[calc(50vh-96px)]">
+                <Carousel data={newsData} />
+            </div>
+            <div className="border border-red-600 w-full h-[calc(50vh-96px)]">
+                <Carousel data={newsData} />
+            </div>
         </div>
-    )
-}
+    );
+};
 
 export default Home;
