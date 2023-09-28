a566a1146263b8df108289f9add3644f9fea6fc1
Azimjon-m <azimeliboev2000@gmail.com>
2023-09-28 16:19:51 +0500

    + AdminPanel Page + Password.jsx

---
+++ src/pages/AdminsPanel/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AdminPanel from '../../components/AdminPanel'
+
+const AdminsPanel = () => {
+    return (
+        <div>
+            <AdminPanel />
+        </div>
+    )
+}
+
+export default AdminsPanel
---
+++ src/passwords.jsx
@@ -0,0 +1,5 @@
+const Passwords = [
+    {id: "12345", password: "12345"},
+    {id: "a", password: "a"},
+]
+export default Passwords;
