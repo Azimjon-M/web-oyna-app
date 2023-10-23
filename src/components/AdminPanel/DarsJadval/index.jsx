e582810671d6f04893790f06962a2c2e3c625cb0
Azimjon <120557486+Azimjon-M@users.noreply.github.com>
2023-10-23 09:57:48 +0500

    TalimTur + Edit

--- src/components/AdminPanel/DarsJadval/index.jsx
+++ src/components/AdminPanel/DarsJadval/index.jsx
@@ -4,16 +4,14 @@ import Fakultet from "./Fakultet";
 import Yonalish from "./Yonalish";
 import Kurs from "./Kurs";
 
-const DarsJadval = ({dataTalim, dataFakultet, dataYonalish, dataKurs}) => {
-    console.log(dataTalim);
-    
+const DarsJadval = ({dataTalim, dataFakultet, dataYonalish, dataKurs}) => {    
     return (
         <div className="px-2 py-10">
             <div className="flex flex-col items-center">
-                <TalimTur />
-                <Fakultet />
-                <Yonalish />
-                <Kurs />
+                <TalimTur dataTalim={dataTalim} />
+                <Fakultet dataTalim={dataTalim} dataFakultet={dataFakultet} />
+                <Yonalish dataTalim={dataTalim} dataFakultet={dataFakultet} dataYonalish={dataYonalish}/>
+                <Kurs dataTalim={dataTalim} dataFakultet={dataFakultet} dataYonalish={dataYonalish} dataKurs={dataKurs} />
             </div>
         </div>
     );
