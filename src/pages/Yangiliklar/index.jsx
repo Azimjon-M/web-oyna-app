3da9b7fef88d203a35aa79ac3eb1d9dba33a38c2
nodirbek27 <nodirjon0927@gmail.com>
2023-11-01 14:27:39 +0500

    update news

--- src/pages/Yangiliklar/index.jsx
+++ src/pages/Yangiliklar/index.jsx
@@ -4,14 +4,22 @@ import Navbar from '../../components/Navbar';
 import Loader from '../../components/Loader';
 
 const Yangiliklar = () => {
-    const [loading, setLoading] = useState(false);
-    const [news, setNews] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [news, setNews] = useState(null);
     useEffect(() => {
         const loadPost = async () => {
-            setLoading(true);
-            const res = await axios.get("https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=f5d42754217c4adfb2af3219e7508901");
-            setNews(res.data.articles);
-            setLoading(false);
+            try {
+                await axios.get("http://api.kspi.uz/v1/yangilik/yangilik/").then(res => {
+                    setNews(res.data);
+                    setLoading(false);
+                }).catch(err => {
+                    console.log(err);
+                    setLoading(false);
+                });
+            } catch (error) {
+                console.log(error);
+                setLoading(false);
+            }
         }
         loadPost();
     }, []);
@@ -24,26 +32,17 @@ const Yangiliklar = () => {
                     {loading ? <Loader /> :
                         <div className='mx-3 my-5 text-center'>
                             {
-                                news && news.map((item, idx) => {
-                                    const text = item.description;
-                                    let modifiedText = "";
-                                    if (text.length > 150) {
-                                        modifiedText += text.slice(0, 150) + "... ";
-                                    } else {
-                                        modifiedText += text + " ";
-                                    }
-                                    return (
+                                news && news.map((item, idx) => (
                                         <div className='my-3 shadow-lg shadow-indigo-500/50' key={idx}>
                                             <div className="w-full flex items-center bg-gradient-to-r from-cyan-100 to-blue-200 border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-                                                <img src={item.urlToImage} className="m-5 object-cover w-[40%] rounded-t-lg h-60" alt="..." />
+                                                <img src={item.rasm} className="m-5 object-cover w-[40%] rounded-t-lg h-60" alt="..." />
                                                 <div className="flex flex-col justify-between p-4 leading-normal w-[60%]">
                                                     <h5 className="mb-2 text-3xl font-bold tracking-tight text-gray-900 dark:text-white">{item.title}</h5>
-                                                    <p className="mb-3 text-lg font-normal text-gray-700 dark:text-gray-400">{modifiedText.trim()}</p>
+                                                    <p className="mb-3 text-lg font-normal text-gray-700 dark:text-gray-400">{item.body}</p>
                                                 </div>
                                             </div>
                                         </div>
-                                    )
-                                })
+                                ))
                             }
                         </div>
                     }
