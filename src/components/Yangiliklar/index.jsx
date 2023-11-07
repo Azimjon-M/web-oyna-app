87f077f9fe339174b36cee12dd5de408f7954d3e
Azimjon <120557486+Azimjon-M@users.noreply.github.com>
2023-11-07 13:55:10 +0500

    update Yangilik

--- src/components/Yangiliklar/index.jsx
+++ src/components/Yangiliklar/index.jsx
@@ -11,6 +11,8 @@ import { Autoplay, EffectCoverflow, Pagination } from 'swiper/modules';
 const Yangiliklar = () => {
     const [loading, setLoading] = useState(true);
     const [news, setNews] = useState(null);
+    const cutTitle = 80;
+    const cutBody = 300;
     useEffect(() => {
         const loadPost = async () => {
             try {
@@ -31,37 +33,48 @@ const Yangiliklar = () => {
 
     return (
         <>
-            <Swiper
-                loop={true}
-                effect={'coverflow'}
-                grabCursor={true}
-                centeredSlides={true}
-                slidesPerView={'auto'}
-                coverflowEffect={{
-                    rotate: 0,
-                    stretch: 0,
-                    depth: 300,
-                    modifier: 1,
-                    slideShadows: true,
-                }}
-                autoplay={{
-                    delay: 2500,
-                    disableOnInteraction: false,
-                }}
-                pagination={{
-                    clickable: true,
-                }}
-                modules={[Autoplay, EffectCoverflow, Pagination]}
-                className="mySwiper w-[100%] h-auto border border-red-600 py-[50px!important]"
-            >
-                {news && news.map((item, idx) => (
-                    <SwiperSlide className='w-[800px!important] flex justify-center rounded-3xl border border-blue-600 overflow-hidden' key={idx}>
-                        <img className='block w-[800px] h-[1200px] rounded-3xl object-cover' src={item.rasm} alt="" />
-                        <h1 className='text-5xl text-center heading1 py-5'>{item.title}</h1>
-                        <p className='text-2xl text-center pb-5'>{item.body}</p>
-                    </SwiperSlide>
-                ))}
-            </Swiper>
+            {
+                loading ? (
+                    <Loader />
+                ) : (
+                    <Swiper
+                        loop={true}
+                        effect={'coverflow'}
+                        grabCursor={true}
+                        centeredSlides={true}
+                        slidesPerView={'auto'}  
+                        coverflowEffect={{
+                            rotate: 0,
+                            stretch: 0,
+                            depth: 300,
+                            modifier: 1,
+                            slideShadows: true,
+                        }}
+                        autoplay={{
+                            delay: 2500,
+                            disableOnInteraction: false,
+                        }}
+                        pagination={{
+                            clickable: true,
+                        }}
+                        modules={[Autoplay, EffectCoverflow, Pagination]}
+                        className="mySwiper w-[100%] h-auto py-[50px!important]"
+                    >
+                        {news && news.map((item, idx) => (
+                            <SwiperSlide className='w-[800px!important] bg-white flex justify-center rounded-3xl overflow-hidden' key={idx}>
+                                <div>
+                                <img className='block w-[800px] h-[1200px] rounded-t-3xl object-cover' src={item.rasm} alt="yangilik img" />
+
+                                </div>
+                                <div>
+                                <h1 className='text-3xl text-center py-5'>{item.title.length > cutTitle ? item.title.slice(0, cutTitle) + "...": item.title}</h1>
+                                <p className='text-2xl text-center pb-5'>{item.body.length > cutBody ? item.body.slice(0, cutBody) + "...": item.body}</p>
+                                </div>
+                            </SwiperSlide>
+                        ))}
+                    </Swiper>
+                )
+            }
         </>
     );
 }
