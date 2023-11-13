48b864f3098a5050eb8d7c2ef0d34405f45a4e25
Azimjon <120557486+Azimjon-M@users.noreply.github.com>
2023-11-13 16:25:09 +0500

    add z index to bg

--- src/root/index.js
+++ src/root/index.js
@@ -6,7 +6,7 @@ const root = () => {
     return (
         <div className='relative'>
             <Outlet />
-            <img className='h-[100vh] w-auto absolute top-0 left-0 border-4 border-blue-600' src={BgImage} />
+            <img className='h-[100vh] w-auto absolute top-0 left-0 border-4 border-blue-600 -z-20' src={BgImage} />
         </div>
     )
 }
