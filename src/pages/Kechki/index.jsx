1f158cdf712da5d25a32f02dea63bbfacc6a8186
nodirbek27 <nodirjon0927@gmail.com>
2023-11-24 16:43:00 +0500

    update kechki admin panel

--- src/pages/Kechki/index.jsx
+++ src/pages/Kechki/index.jsx
@@ -2,7 +2,12 @@ import React from "react";
 import KechkiComp from "../../components/Kechki";
 
 const Kechki = () => {
-    return <div><KechkiComp /></div>;
+    return (
+        <div>
+            <h1 className="text-4xl font-bold text-slate-600 text-center mt-10">Kechki ta'lim</h1>
+            <KechkiComp />
+        </div>
+    )
 };
 
 export default Kechki;
