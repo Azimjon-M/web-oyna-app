cf463b650b51a7e3500a7147b92cd21a4bd81335
nodirbek27 <nodirjon0927@gmail.com>
2023-11-29 09:23:43 +0500

    update interactivaHizmat

--- src/components/InterActiveHiz/index.jsx
+++ src/components/InterActiveHiz/index.jsx
@@ -29,28 +29,29 @@ const InterActiveHiz = () => {
     const institutXizmatlari = () => {
         setInfoContent(
             <div className='w-[1000px] h-[850px] mx-auto'>
-                <h2 className='text-5xl mb-[80px] text-center font-medium'>Institut interaktiv xizmatlari</h2>
+                <h2 className='text-5xl mb-[30px] text-center font-medium'>Institut interaktiv xizmatlari</h2>
+                <h2 className='text-4xl mb-[50px] text-center font-medium'>(Talabalar uchun)</h2>
                 <div className='flex justify-center items-center flex-wrap'>
                     <div className='flex justify-center items-center flex-col w-[500px] mb-[80px]'>
-                        <h3 className='text-3xl font-medium mt-5 mb-[60px]'>Talaba amaliyot shartnoma</h3>
+                        <h3 className='text-3xl font-medium mt-5 mb-[60px]'>Amaliyot shartnoma</h3>
                         <div className='mx-auto w-[150px] h-[150px]'>
                             <QRCode className='bg-white border-4 border-slate-700 p-3' value={urlSharnomaAmal} />
                         </div>
                     </div>
                     <div className='flex justify-center items-center flex-col w-[500px] mb-[80px]'>
-                        <h3 className='text-3xl font-medium mt-5 mb-[60px]'>Talaba kontrakt shartnoma</h3>
+                        <h3 className='text-3xl font-medium mt-5 mb-[60px]'>Kontrakt shartnoma</h3>
                         <div className='mx-auto w-[150px] h-[150px]'>
                             <QRCode className='bg-white border-4 border-slate-700 p-3' value={urlSharnomaKant} />
                         </div>
                     </div>
                     <div className='flex justify-center items-center flex-col w-[500px]'>
-                        <h3 className='text-3xl font-medium mt-5 mb-[60px]'>Talaba turar-joy shartnoma</h3>
+                        <h3 className='text-3xl font-medium mt-5 mb-[60px]'>TTJga ariza berish</h3>
                         <div className='mx-auto w-[150px] h-[150px]'>
                             <QRCode className='bg-white border-4 border-slate-700 p-3' value={urlTurarjoy} />
                         </div>
                     </div>
                     <div className='flex justify-center items-center flex-col w-[500px]'>
-                        <h3 className='text-3xl font-medium mt-5 mb-[60px]'>Talaba o'qish joyidan ma'lumotnoma</h3>
+                        <h3 className='text-3xl font-medium mt-5 mb-[60px]'>O'qish joyidan ma'lumotnoma</h3>
                         <div className='mx-auto w-[150px] h-[150px]'>
                             <QRCode className='bg-white border-4 border-slate-700 p-3' value={urloqishMal} />
                         </div>
