548409f4b3e4df0099d39fe802c77f4b8d1edc0a
nodirbek27 <nodirjon0927@gmail.com>
2023-12-26 08:53:43 +0500

    add comment to reklama section

--- src/components/CarouselReklama/index.jsx
+++ src/components/CarouselReklama/index.jsx
@@ -4,7 +4,11 @@ const VideoCarouselReclama = () => {
 
     return (
         <>
-            <p>hi</p>
+            <div className="bg-[#222] h-full text-white text-center py-5 text-xl">
+                <p className="bg-[#444] mb-5 px-20 py-3">Har qanday millatni halokatga olib borish uchun atom bombalari va uzoq masofaga mo'ljallangan raketalar kerak emas</p>
+                <p className="bg-[#444] mb-5 px-20 py-3">Ta'limning sifatining zaifligi va o'quvchilarni imtihonlardagi <br /> aldovlariga yo'l qo'yishigina kifoyadir</p>
+                <p className="bg-[#444] px-20 py-3">Bemorlar shunday vrachlar qo'lida halok bo'ladi. Binolar shunday injinerlar va arxitektorlar tufayli qulab tushadi. Pul va mablag'lar shunday iqtisodchilar va hisobchilar qo'lida yo'q bo'ladi. Adolat shunday yuristlar va sudyalar tomonidan barham topadi. <br /> Ta'limning halokati - millatning halokatidir.</p>
+            </div>
         </>
     );
 };
