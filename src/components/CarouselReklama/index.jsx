38cd161f62bbd454cec1f9ce679d81537a9c520c
nodirbek27 <nodirjon0927@gmail.com>
2023-12-27 08:47:40 +0500

    change text

--- src/components/CarouselReklama/index.jsx
+++ src/components/CarouselReklama/index.jsx
@@ -4,10 +4,10 @@ const VideoCarouselReclama = () => {
 
     return (
         <>
-            <div className="bg-[#222] h-full text-white text-center py-5 text-xl">
-                <p className="bg-[#444] mb-5 px-20 py-3">Har qanday millatni halokatga olib borish uchun atom bombalari va uzoq masofaga mo'ljallangan raketalar kerak emas</p>
-                <p className="bg-[#444] mb-5 px-20 py-3">Ta'limning sifatining zaifligi va o'quvchilarni imtihonlardagi <br /> aldovlariga yo'l qo'yishigina kifoyadir</p>
-                <p className="bg-[#444] px-20 py-3">Bemorlar shunday vrachlar qo'lida halok bo'ladi. Binolar shunday injinerlar va arxitektorlar tufayli qulab tushadi. Pul va mablag'lar shunday iqtisodchilar va hisobchilar qo'lida yo'q bo'ladi. Adolat shunday yuristlar va sudyalar tomonidan barham topadi. <br /> Ta'limning halokati - millatning halokatidir.</p>
+            <div className="bg-[#222] h-full text-white flex items-center text-center py-5 text-2xl">
+                <p className="bg-[#444] px-20 py-3">Har qanday millatni halokatga olib borish uchun atom bombalari va uzoq masofaga mo'ljallangan raketalar kerak emas. <br />
+                Ta'limning sifatining zaifligi va o'quvchilarni imtihonlardagi aldovlariga yo'l qo'yishigina kifoyadir.
+                Bemorlar shunday shifokorlar qo'lida halok bo'ladi. Binolar shunday muhandislar va arxitektorlar tufayli qulab tushadi. Pul va mablag'lar shunday iqtisodchilar va hisobchilar qo'lida yo'q bo'ladi. Adolat shunday huquqshunoslar va sudyalar tomonidan barham topadi. <br /> Ta'limning halokati - millatning halokatidir.</p>
             </div>
         </>
     );
